6c8653667f59bc80526c562d0844f31873eaa6f9
Simon Ammer <ammersimon@gmail.com>
2018-04-17 11:11:03 +0200

    🚧 Add EV3infrared sensor support

---
+++ src/main/kotlin/com/angrynerds/ev3/lejos/sensors/RxEV3IRSensor.kt
@@ -0,0 +1,20 @@
+package com.angrynerds.ev3.lejos.sensors
+
+import io.reactivex.Observable
+import lejos.hardware.port.Port
+import lejos.hardware.sensor.EV3IRSensor
+
+class RxEV3IRSensor(port: Port) {
+
+    private val distance: Observable<Float>
+
+    init {
+        distance = Observable.using(
+                { EV3IRSensor(port) },
+                { sensor: EV3IRSensor -> Sampler(sensor.distanceMode).sample },
+                { it.close() })
+                .share()
+                .map { sample -> sample.values[sample.offset] }
+                .distinctUntilChanged()
+    }
+}
