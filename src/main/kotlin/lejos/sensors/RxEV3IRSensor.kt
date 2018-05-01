0eedfd1bee80d80cbcac6db89651e2ed14f2d4a6
Simon Ammer <ammersimon@gmail.com>
2018-05-01 17:09:23 +0200

    💥 Add multiple constructors to RxEV3IRSensor

--- src/main/kotlin/lejos/sensors/RxEV3IRSensor.kt
+++ src/main/kotlin/lejos/sensors/RxEV3IRSensor.kt
@@ -4,15 +4,31 @@ import io.reactivex.Observable
 import lejos.hardware.port.Port
 import lejos.hardware.sensor.EV3IRSensor
 
-class RxEV3IRSensor(port: Port) {
+class RxEV3IRSensor {
 
+    private var port: Port? = null
+    private var sensor: EV3IRSensor? = null
     var distance: Observable<Float> private set
 
-    init {
+    constructor(port: Port, autoClose: Boolean = true)
+    {
+        this.port = port
         distance = Observable.using(
                 { EV3IRSensor(port) },
-                { sensor: EV3IRSensor -> Sampler(sensor.distanceMode).sample },
-                { it.close() })
+                { irSensor: EV3IRSensor -> Sampler(irSensor.distanceMode).sample },
+                { if (autoClose) it.close() })
+                .share()
+                .map { sample -> sample.values[sample.offset] }
+                .distinctUntilChanged()
+    }
+
+    constructor(sensor: EV3IRSensor, autoClose: Boolean = true)
+    {
+        this.sensor = sensor
+        distance = Observable.using(
+                { sensor },
+                { irSensor: EV3IRSensor -> Sampler(irSensor.distanceMode).sample },
+                { if (autoClose) it.close() })
                 .share()
                 .map { sample -> sample.values[sample.offset] }
                 .distinctUntilChanged()
