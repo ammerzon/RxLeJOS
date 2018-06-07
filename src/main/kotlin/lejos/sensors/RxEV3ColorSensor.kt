f836fa0372420b4aefd20ac160926a18e6bc29f8
Simon Ammer <ammersimon@gmail.com>
2018-06-07 10:05:23 +0200

    🔥 Remove color observable

--- src/main/kotlin/lejos/sensors/RxEV3ColorSensor.kt
+++ src/main/kotlin/lejos/sensors/RxEV3ColorSensor.kt
@@ -3,14 +3,12 @@ package lejos.sensors
 import io.reactivex.Observable
 import lejos.hardware.port.Port
 import lejos.hardware.sensor.EV3ColorSensor
-import lejos.robotics.Color
 
 class RxEV3ColorSensor {
 
     private var port: Port? = null
     private var sensor: EV3ColorSensor? = null
     var colorId: Observable<ColorId> private set
-    var color: Observable<Color> private set
 
     constructor(port: Port, autoClose: Boolean = true) {
         colorId = Observable.using(
@@ -20,19 +18,6 @@ class RxEV3ColorSensor {
                 .share()
                 .map { sample -> sample.values[sample.offset] }
                 .map { value -> ColorId.colorId(value) }
-                .distinctUntilChanged()
-
-        color = Observable.using(
-                { EV3ColorSensor(port) },
-                { sensor: EV3ColorSensor -> Sampler(sensor.rgbMode).sample },
-                { if (autoClose) it.close() })
-                .share()
-                .map { sample ->
-                    Color(
-                            Math.round(sample.values[sample.offset]),
-                            Math.round(sample.values[sample.offset + 1]),
-                            Math.round(sample.values[sample.offset + 2]))
-                }
                 .distinctUntilChanged()
     }
 
@@ -45,19 +30,6 @@ class RxEV3ColorSensor {
                 .share()
                 .map { sample -> sample.values[sample.offset] }
                 .map { value -> ColorId.colorId(value) }
-                .distinctUntilChanged()
-
-        color = Observable.using(
-                { sensor },
-                { colorSensor: EV3ColorSensor -> Sampler(colorSensor.rgbMode).sample },
-                { if (autoClose) it.close() })
-                .share()
-                .map { sample ->
-                    Color(
-                            Math.round(sample.values[sample.offset]),
-                            Math.round(sample.values[sample.offset + 1]),
-                            Math.round(sample.values[sample.offset + 2]))
-                }
                 .distinctUntilChanged()
     }
 }
